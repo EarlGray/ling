9349be37332e606a5934b1cc4883a3bdd54ed12c
Dmytro S <dmytrish@gmail.com>
2015-06-04 18:08:29 +0300

    pic32: +cluster.c ; arch/console.c enhancements

--- core/arch/mips/console.c
+++ core/arch/mips/console.c
@@ -1,17 +1,30 @@
+#include <stdbool.h>
 
 #include "ling_common.h"
+
+static struct {
+    bool is_initialized;
+} the_console = {
+    .is_initialized = false
+};
 
 void console_init()
 {
     DBINIT();
+    the_console.is_initialized = true;
+}
+
+int console_is_initialized(void) {
+    return (int)the_console.is_initialized;
 }
 
 int ser_cons_write(char *buf, int len)
 {
 #if defined(__ENABLEIO_STARTERKIT_DEBUG)
+    if (!the_console.is_initialized) return -1;
     db_puts(buf, len);
 #else
-# error "no __ENABLEIO_STARTERKIT_DEBUG"
+# warning "No ser_cons_write! printk() is disabled"
 #endif
     return 0;
 }
