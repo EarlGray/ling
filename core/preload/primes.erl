3e89d487478241ce0af47227f235c27ceeda6287
Dmytro S <dmytrish@gmail.com>
2015-06-23 21:06:09 +0300

    pic32: primes:up_to(20) works

--- core/preload/primes.erl
+++ core/preload/primes.erl
@@ -2,16 +2,49 @@
 -compile(export_all).
 -export([up_to/1, exclude/3]).
 
+%%
+%%    A small excerpt from lists.erl
+%%
+
+seq(First, Last) when is_integer(First), is_integer(Last), First-1 =< Last -> 
+    seq_loop(Last-First+1, Last, []).
+
+seq_loop(N, X, L) when N >= 4 ->
+     seq_loop(N-4, X-4, [X-3,X-2,X-1,X|L]);
+seq_loop(N, X, L) when N >= 2 ->
+     seq_loop(N-2, X-2, [X-1,X|L]);
+seq_loop(1, X, L) ->
+     [X|L];
+seq_loop(0, _, L) ->
+     L.
+
+dropwhile(Pred, [Hd|Tail]=Rest) ->
+    case Pred(Hd) of
+	true -> dropwhile(Pred, Tail);
+	false -> Rest
+    end;
+dropwhile(Pred, []) when is_function(Pred, 1) -> [].
+
+
+%
+% helper routine
+%
 exclude(_, _, []) -> [];
 exclude(N, By, [H | T]) when N == H -> exclude(By + N, By, T);
 exclude(N, By, [H | T]) when H < N -> [H | exclude(N, By, T)];
 exclude(N, By, [H | T]) -> [H | exclude(N + By, By, T)].
 
-up_to(N) -> sieve(lists:seq(2, N), 2).
+
+%%
+%% interface
+%%
 
 sieve(Lst, P) ->
   Lst1 = exclude(P + P, P, Lst),
-  case lists:dropwhile(fun(X) -> X =< P end, Lst1) of
+  case dropwhile(fun(X) -> X =< P end, Lst1) of
     [] -> Lst1;
     [ P1 | _ ] -> sieve(Lst1, P1)
   end.
+
+up_to(N) -> sieve(seq(2, N), 2).
+
